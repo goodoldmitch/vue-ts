92ea6535a412bcd52d66434ec984048fd3c6fe3a
Sergey Kuzin <sergey.kuzin@lemurrr.com>
2025-07-02 14:11:57 +0400

    Basic vue counter

---
+++ vue-ts-counter/src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const countNumber  = ref(0);
+
+  function increment(){countNumber.value++}
+  function reset(){countNumber.value = 0}
+  function decrement(){countNumber.value--}
+</script>
+
+<template>
+  
+  <h1>{{ countNumber }}</h1>
+  <div class="buttons-block">
+    <button @click="decrement()" class="decrement">decrement</button>
+    <button @click="reset()" class="reset">reset</button>
+    <button @click="increment()" class="increment">increment</button>
+  </div>
+</template>
+
+<style scoped>
+  h1{
+    font-size: 160px;
+    text-align: center;
+    margin-bottom: 40px;
+  }
+  button{
+    border: none;
+    padding: 10px 15px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+  button.decrement{
+    background: pink;
+  }
+  button.reset{
+    background: antiquewhite;
+  }
+  button.increment{
+    background: lightblue;
+  }
+</style>
